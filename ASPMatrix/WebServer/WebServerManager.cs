518bda60e554a08273decf7d44a27d4aee8b059b
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2023-05-06 01:02:07 +0100

    Removed old code

--- ASPMatrix/WebServer/WebServerManager.cs
+++ ASPMatrix/WebServer/WebServerManager.cs
@@ -85,67 +85,6 @@ public class WebServerManager : IHostedService, IDisposable
         await _proxyConfigurator.SetProxyRules(rules);
     }
 
-    // private async Task SetWebServers(IList<WebServerConfig> webServerConfigs)
-    // {
-    //     var stopInstances = GetStopServices(webServerConfigs).ToArray();
-    //     var tasks = new List<Task>();
-    //     foreach(var instance in stopInstances)
-    //     {
-    //         _logger.LogInformation($"Stopping {instance.WebServerConfig.Name} on {instance.WebServerConfig.UrlPath}");
-    //         var task = instance.WebServer.Stop();
-    //         tasks.Add(task);
-
-    //         _webServerInstances.Remove(instance);
-    //     }
-
-    //     await Task.WhenAll(tasks.ToArray());
-
-    //     var startServices = webServerConfigs
-    //         .Where(x => _webServerInstances.All(y => y.WebServerConfig.UrlPath != x.UrlPath))
-    //         .ToArray();
-    //     foreach(var config in startServices)
-    //     {
-    //         WebServerInstance instance;
-    //         try
-    //         {
-    //             instance = CreateWebServerInstance(config);
-    //             _logger.LogInformation($"Starting {instance.WebServerConfig.Name} on {instance.WebServerConfig.UrlPath}");
-    //             await instance.WebServer.Start();
-    //         }
-    //         catch (Exception e)
-    //         {
-    //             _logger.LogError($"Failed to start {config.Name}: {e.Message}");
-    //             continue;
-    //         }
-
-    //         _webServerInstances.Add(instance);
-    //     }
-
-    //     if (stopInstances.Any() || startServices.Any())
-    //     {
-    //         var rules =_webServerInstances.Select(x => new ProxyRule(x.WebServerConfig.UrlPath, x.Port));
-    //         await _proxyConfigurator.SetProxyRules(rules);
-    //     }
-    // }
-
-    // private IEnumerable<WebServerInstance> GetStopServices(IList<WebServerConfig> webServerConfigs)
-    // {
-    //     foreach(var instance in _webServerInstances)
-    //     {
-    //         var config = webServerConfigs.FirstOrDefault(x => x.UrlPath == instance.WebServerConfig.UrlPath);
-    //         if (config == null)
-    //         {
-    //             yield return instance;
-    //             continue;
-    //         }
-
-    //         if (config.ServicePath != instance.WebServerConfig.ServicePath || config.Name != instance.WebServerConfig.Name)
-    //         {
-    //             yield return instance;
-    //         }
-    //     }
-    // }
-
     private int FindPort()
     {
         for (int i = 5000; ; i++)
